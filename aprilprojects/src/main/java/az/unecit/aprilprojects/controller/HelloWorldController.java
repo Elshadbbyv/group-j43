2c07037a33df9f0fa2090abc940df692dfdff1eb
elshadbbyv <babayevelshad09@gmail.com>
2022-03-27 01:19:05 +0400

    first

---
+++ aprilprojects/src/main/java/az/unecit/aprilprojects/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package az.unecit.aprilprojects.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping
+    public String hello(){
+        return "Hello Spring World";
+    }
+}
