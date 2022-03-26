da5df76f46051337bf2d6e6e17c3946b5398ba03
elshadbbyv <babayevelshad09@gmail.com>
2022-03-26 22:31:06 +0400

    first project-aze

---
+++ aprilprojects/src/main/java/az/unecit/aprilprojects/AprilprojectsApplication.java
@@ -0,0 +1,13 @@
+package az.unecit.aprilprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprilprojectsApplication.class, args);
+    }
+
+}
